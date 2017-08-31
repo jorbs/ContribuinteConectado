d149d8a62a69aba52b31e6a0a5885744e8753486
João Roberto <joaorobertojr88@gmail.com>
2017-08-31 11:54:28 -0300

    Postos

---
+++ app/common/Postos.js
@@ -0,0 +1,40 @@
+const Postos = [
+  {
+    key: 'SERRADASPIAS',
+    name: 'Posto Fiscal - Maragogi',
+    address: 'Rodovia AL 101 Norte - KM 19',
+    phone: '(82) 3216-9712 / 3216-9713',
+  },
+  {
+    key: 'SERRADASPIAS',
+    name: 'Posto Fiscal - Novo Lino',
+    address: 'Rodovia BR 101 - KM 0',
+    phone: '(82) 3216-9708 / 3216-9709 / 99889088',
+  },
+  {
+    key: 'SERRADASPIAS',
+    name: 'Posto Fiscal - São José da Laje',
+    address: 'Rodovia BR 104 - KM 12',
+    phone: '(82) 3216-9706 / 3216-9707 / 3666-1953',
+  },
+  {
+    key: 'SERRADASPIAS',
+    name: 'Posto Fiscal - Palmeira dos Índios',
+    address: 'Serra das Pias (Compartilhado com o P.F. Bom Conselho - PE)',
+    phone: 'Fone: (87) 3771-3902 / Fax: (87) 3771-3903',
+  },
+  {
+    key: 'SERRADASPIAS',
+    name: 'Posto Fiscal - Porto Real do Colégio',
+    address: 'Rodovia BR 101 - KM 245',
+    phone: '(82) 3553-1169 / 8833-9232 / 3553-1650',
+  },
+  {
+    key: 'SERRADASPIAS',
+    name: 'Posto Fiscal - Delmiro Gouveia',
+    address: 'Rodovia AL 423, KM 101',
+    phone: '(82) 3216-97000 / 3216-9701 / 9902-1853',
+  }
+];
+
+export default Postos;
